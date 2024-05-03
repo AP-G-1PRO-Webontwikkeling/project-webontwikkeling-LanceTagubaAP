ee8fddef845d50673dbc23b7dffd0ea82cbf5c28
Taguba Lance (s143847) <lance.taguba@student.ap.be>
2024-05-03 10:43:38 +0000

    table css

--- index.ts
+++ index.ts
@@ -2,69 +2,160 @@ import * as readline from 'readline-sync';
 import director from "./director.json";
 //import data from "./movies.json";
 import { Movie } from './types';
+import express from "express";
 
-const directors = director;
-//const movies : Movie[] = data;
 
-  function displaymovie(movie: Movie): void {
-    console.log(`\n- ${movie.title} (${movie.id})`);
-    console.log(`  - Description: ${movie.description}`);
-    console.log(`  - Release year: ${movie.release_year}`);
-    if (movie.is_downloadable) {
-        console.log(`  - Downloadable: Yes`);
-    } else {
-        console
-        .log(`  - Downloadable: No`);
-    }
-    console.log(`  - Release Date: ${movie.release_date}`);
-    console.log(`  - Image url: ${movie.release_year}`);
-    console.log(`  - Genre: ${movie.genre}`);
-    console.log(`  - Cast: ${movie.cast.join(", ")}`);
-    console.log(`  - Director: ${movie.director.name}
-                       Birthdate: ${movie.director.birth_date}
-                       Id: ${movie.director.id}`);
+
+const app = express();
+
+app.set("port", 3000);
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+let movies : Movie[] = [];
+
+app.get("/", (req, res) => {
+    /**Hier komt eerste pagina */
+
+    const sortField = typeof req.query.sortField === "string" ? req.query.sortField : "name";
+    const sortDirection = typeof req.query.sortDirection === "string" ? req.query.sortDirection : "asc";
     
-    
-  } 
-function displayMenu(): void {
-  
-  console.log("1. View all data");
-  console.log("2. Filter by ID");
-  console.log("3. Exit");
-}
 
- async function main(){
-    const response = await fetch("https://raw.githubusercontent.com/AP-G-1PRO-Webontwikkeling/project-webontwikkeling-LanceTagubaAP/main/movies.json?token=GHSAT0AAAAAACNZ6QZAA3YUO5RVIWWBQH6EZPBJC7Q");
-    const movies : Movie[]= await response.json();
-    
-    console.log("Welcome to the JSON data viewer!\n");
-    
-    let choice : number;
-    do {
-        displayMenu();
-        choice = Number(readline.question("Please enter a choice "));
 
-        switch (choice) {
-            case 1:
-                movies.forEach(element => {
-                    console.log(`${element.title} (Id:${element.id})`)
-                });
-                
-                break;
-            case 2:
-                
-                let idChosen : number = Number(readline.question("Please enter an Id "));
-                displaymovie(movies[idChosen -1]);
-                break;
-
-            case 3:
-                break;    
-            default:
-                console.log("foute keuze")
-                break;
+    let sortedFilms = [...movies].sort((a, b) => {
+        if (sortField === "name") {
+            return sortDirection === "asc" ? a.title.localeCompare(b.title) : b.title.localeCompare(a.title);
+        } else if (sortField === "releasteDate") {
+            return sortDirection === "asc" ? a.release_year - b.release_year : b.release_year - a.release_year;
+        } else if (sortField === "director") {
+            return sortDirection === "asc" ? a.director.name.localeCompare(b.director.name) : b.director.name.localeCompare(a.director.name);
+            
+        }else if (sortField === "genre") {
+            return sortDirection === "asc" ? a.genre.localeCompare(b.genre) : b.genre.localeCompare(a.genre);
+            
         }
-   
-   
-    } while (choice != 3);
-}
-main();
+         else {
+            return 0;
+        }
+    });
+
+    const sortFields = [
+        { value: 'name', text: 'Name', selected: sortField === 'name' ? 'selected' : '' },
+        { value: 'releasteDate', text: 'Release Date', selected: sortField === 'releaseDate' ? 'selected' : '' },
+        
+       
+        { value: 'director', text: 'Director', selected: sortField === 'director' ? 'selected' : ''},
+        { value: 'genre', text: 'Genre', selected: sortField === 'genre' ? 'selected' : ''},
+        { value: 'downloadable', text: 'Downloadable', selected: sortField === 'downloadable' ? 'selected' : '' }
+
+    ];
+
+    const sortDirections = [
+        { value: 'asc', text: 'Ascending', selected: sortDirection === 'asc' ? 'selected' : ''},
+        { value: 'desc', text: 'Descending', selected: sortDirection === 'desc' ? 'selected' : ''}
+    ];
+
+
+
+
+
+
+
+
+    res.render("index",{
+        films : sortedFilms,
+        sortFields : sortFields,
+        sortDirections: sortDirections,
+        sortField: sortField,
+        sortDirection: sortDirection
+    });
+});
+
+
+
+
+app.listen(app.get("port"), async () => {
+    console.log("[server] http://localhost:" + app.get("port"));
+    const response = await fetch("https://raw.githubusercontent.com/AP-G-1PRO-Webontwikkeling/project-webontwikkeling-LanceTagubaAP/main/movies.json?token=GHSAT0AAAAAACNZ6QZAA3YUO5RVIWWBQH6EZPBJC7Q");
+    movies = await response.json();
+    
+
+    }
+);
+
+
+
+
+
+
+
+
+
+
+
+
+// const directors = director;
+// //const movies : Movie[] = data;
+
+// function displaymovie(movie: Movie): void {
+//     console.log(`\n- ${movie.title} (${movie.id})`);
+//     console.log(`  - Description: ${movie.description}`);
+//     console.log(`  - Release year: ${movie.release_year}`);
+//     if (movie.is_downloadable) {
+//         console.log(`  - Downloadable: Yes`);
+//     } else {
+//         console
+//             .log(`  - Downloadable: No`);
+//     }
+//     console.log(`  - Release Date: ${movie.release_date}`);
+//     console.log(`  - Image url: ${movie.release_year}`);
+//     console.log(`  - Genre: ${movie.genre}`);
+//     console.log(`  - Cast: ${movie.cast.join(", ")}`);
+//     console.log(`  - Director: ${movie.director.name}
+//                        Birthdate: ${movie.director.birth_date}
+//                        Id: ${movie.director.id}`);
+
+
+// }
+// function displayMenu(): void {
+
+//     console.log("1. View all data");
+//     console.log("2. Filter by ID");
+//     console.log("3. Exit");
+// }
+
+// async function main() {
+//     const response = await fetch("https://raw.githubusercontent.com/AP-G-1PRO-Webontwikkeling/project-webontwikkeling-LanceTagubaAP/main/movies.json?token=GHSAT0AAAAAACNZ6QZAA3YUO5RVIWWBQH6EZPBJC7Q");
+//     const movies: Movie[] = await response.json();
+
+//     console.log("Welcome to the JSON data viewer!\n");
+
+//     let choice: number;
+//     do {
+//         displayMenu();
+//         choice = Number(readline.question("Please enter a choice "));
+
+//         switch (choice) {
+//             case 1:
+//                 movies.forEach(element => {
+//                     console.log(`${element.title} (Id:${element.id})`)
+//                 });
+
+//                 break;
+//             case 2:
+
+//                 let idChosen: number = Number(readline.question("Please enter an Id "));
+//                 displaymovie(movies[idChosen - 1]);
+//                 break;
+
+//             case 3:
+//                 break;
+//             default:
+//                 console.log("foute keuze")
+//                 break;
+//         }
+
+
+//     } while (choice != 3);
+// }
+// main();
